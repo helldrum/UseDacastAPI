9ab2900c537ab74c8b7d18e33624b216eb2bc2c9
Helldrum <jonathanchardon@hotmail.fr>
2014-06-30 11:50:31 -0700

    fix ssl certificate

--- SingleAPIFunction/VOD/getEmbedCodeVOD.php
+++ SingleAPIFunction/VOD/getEmbedCodeVOD.php
@@ -4,16 +4,22 @@ define("APIKEY", "YourAPIKey");
 define("URL", "https://www.dacast.com/backend/api/vod");
 define("BID", "yourBroadcasterID");
 
+
+
+
+
 main();
 
 function main() {
-    $vod_id = "YourVODid";
+    $vod_id = "yourVODId";
 
     $ch = curl_init(URL . "/" . $vod_id . "/embed/frame?bid=" . BID . "&apikey=" . APIKEY);
 
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-    curl_setopt($ch, CURLOPT_CAINFO, "cacert.pem");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_setopt($ch, CURLOPT_VERBOSE, 1);
     curl_setopt($ch, CURLOPT_USERAGENT, "MozillaXYZ/1.0");
 
     $output = curl_exec($ch);
@@ -21,7 +27,7 @@ function main() {
     curl_close($ch);
     $decoded = json_decode($output);
 
-    echo var_dump($decoded);
+    echo $decoded;
 }
 ?>
 
